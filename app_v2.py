18b037217ed826c7e3e419ae60a0653917ff5732
1228871882-byte <1228871882@qq.com>
2026-06-29 11:04:39 +0800

    support PORT env

--- app_v2.py
+++ app_v2.py
@@ -429,4 +429,5 @@ def load_questions():
 init()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=False)
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port, debug=False)
